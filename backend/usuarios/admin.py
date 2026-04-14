0d82bc28e2b579fa33d6dc876664d1fe68aedd0a
Lorenzo <lorenzodreis@gmail.com>
2026-04-14 19:52:59 -0300

    adicionando form ao admin de usuario

--- backend/usuarios/admin.py
+++ backend/usuarios/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+from usuarios.forms import UsuarioForm
 from usuarios.models import Usuario
 
 
 # Register your models here.
 @admin.register(Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
+    form = UsuarioForm
     list_display = ("nome", "email", "cargo")
     search_fields = ("nome", "email")
     list_filter = ("cargo",)
